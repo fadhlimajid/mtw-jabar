65418bd5ef907e7786536a3fd77f47c4f00b0e99
Fadhli Majid <fadhlimajid43@gmail.com>
2018-11-18 09:18:03 +0700

    auto hide function

--- public/javascripts/toggle-dropdown.js
+++ public/javascripts/toggle-dropdown.js
@@ -1,13 +1,20 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction(id) {
-    document.getElementById(id).classList.toggle("show");
+  document.getElementById(id).classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
+  const dropdowns = document.getElementsByClassName("dropdown-content");
+
+  for (let i = 0; i < dropdowns.length; i++) {
+    if (!event.target.matches(`.${dropdowns[i].id}`) && dropdowns[i].classList.contains('show')) {
+      dropdowns[i].classList.remove('show');
+    }
+  }
+
   if (!event.target.matches('.dropdown-btn')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
     for (let i = 0; i < dropdowns.length; i++) {
       const openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
